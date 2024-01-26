003c0bf3de006db41fe56a06bb4dc112a0456716
minfuel <106936397+minfuel@users.noreply.github.com>
2024-01-26 11:47:59 +0000

    more base files

--- svelte.config.js
+++ svelte.config.js
@@ -7,20 +7,13 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	/*kit: {
+	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}*/
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
-		}),
-		prerender: { entries: [] }
 	}
+
 };
 
 export default config;
